2e2f735134e19a6a29667bdc58370788e56afe0a
MaskerAW <mnfgaming2@gmail.com>
2020-12-12 20:40:02 +0700

    update 12/12/2020

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-var btnlogin = document.querySelector(".btn-login");
-var ipemail = document.querySelector("#email");
-var ippass = document.querySelector("#pass");
+var ipemail = document.querySelector(".info.email");
+var ippass = document.querySelector(".info.password");
 var alrt = document.querySelector(".alrt");
-btnlogin.addEventListener("click", check);
-alrt.disabled = true;
+var btnpg = document.querySelector(".btn.pg");
+var btnlg = document.querySelector(".btn.lg");
+var logo = document.querySelector("#logo");
+var btnacc = document.querySelector(".btnaccount");
+
+
+logo.addEventListener("click", function () {
+    window.location.href = "main.html";
+})
+
+btnlg.addEventListener("click", check);
+
+btnpg.addEventListener("click", function () {
+    window.location.href = "post.html";
+})
 
 function check() {
-    if (ipemail.value == "" || ippass.value == "") {
+    if (ipemail.value == null && ippass.value == null) {
         alrt.style.display = "block";
     }
     else {
-        alrt.hide;
+        alrt.style.display = "none";
+        if (ipemail.value == "admin" && ippass.value == "abc") {
+            window.location.href = "post.html";
+        }
     }
 }
